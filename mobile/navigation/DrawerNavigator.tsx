f24f85e93cb6e7023afa4c24863210751907a300
Antony Baasandorj <antony.baasan@gmail.com>
2020-11-08 19:35:32 -0500

    add option

--- mobile/navigation/DrawerNavigator.tsx
+++ mobile/navigation/DrawerNavigator.tsx
@@ -1,12 +1,7 @@
 import * as React from 'react';
-import { Ionicons } from '@expo/vector-icons';
 import i18n from 'i18n-js';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
-import Colors from '../constants/Colors';
-import useColorScheme from '../hooks/useColorScheme';
-import { BottomTabParamList } from '../types';
-import { FindExerciseNavigator } from './stacks/FindExerciseStackNavigator';
 import { SettingsNavigator } from './stacks/SettingsStackNavigator';
 import { DebugNavigator } from './stacks/DebugStackNavigator';
 import BottomTabNavigator from './BottomTabNavigator';
@@ -15,12 +10,20 @@ const Drawer = createDrawerNavigator();
 
 export default function MainDrawer() {
   return (
-    <Drawer.Navigator>
-      <Drawer.Screen name={i18n.t('tab.exercises')} component={BottomTabNavigator} />
-      <Drawer.Screen name={i18n.t('tab.settings')} component={SettingsNavigator} />
+    <Drawer.Navigator
+      options
+    >
+      <Drawer.Screen
+        name="Exercises"
+        component={BottomTabNavigator}
+        options={{ drawerLabel: i18n.t('tab.exercises') }}
+      />
+      <Drawer.Screen
+        name="Settings"
+        component={SettingsNavigator}
+        options={{ drawerLabel: i18n.t('tab.settings') }}
+      />
       <Drawer.Screen name="Debug" component={DebugNavigator} />
     </Drawer.Navigator>
   );
 }
-
-  
