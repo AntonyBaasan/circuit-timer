4e6988238971cd05bbfaa49045f958c54dcbc320
Antony Baasandorj <antony.baasan@gmail.com>
2020-11-18 00:32:09 -0500

    remove unused file

--- mobile/screens/ExerciseListScreen/ExerciseListScreen.tsx
+++ mobile/screens/ExerciseListScreen/ExerciseListScreen.tsx
@@ -1,17 +1,12 @@
-import React, { useEffect, useLayoutEffect } from 'react';
+import React, { useLayoutEffect } from 'react';
 import { StyleSheet, SafeAreaView, FlatList } from 'react-native';
 import i18n from 'i18n-js';
-import { ThemeProvider, Button } from 'react-native-elements';
-import {
-  HeaderButtons,
-  HiddenItem,
-  Item,
-} from 'react-navigation-header-buttons';
+import { ThemeProvider } from 'react-native-elements';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { useSelector } from 'react-redux';
 
 import { ScreenNames } from '../../constants/Screen';
 import { Exercise } from '../../models/exercise';
-import { DEMO_EXERCISE } from '../../data/example';
 import { mainTheme } from '../../constants/theme/Main';
 import ExerciseListItem from './ExerciseListItem';
 import { CustomHeaderButton } from '../../components/navigation/HeaderButtons';
