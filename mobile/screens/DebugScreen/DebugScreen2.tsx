ccef2d5a57627491078c1cf25d8c3ca3f1c15e04
Antony Baasandorj <antony.baasan@gmail.com>
2021-01-10 23:52:39 -0500

    add reanimate example

--- mobile/screens/DebugScreen/DebugScreen2.tsx
+++ mobile/screens/DebugScreen/DebugScreen2.tsx
@@ -1,33 +1,91 @@
 import * as React from 'react';
 import { StyleSheet, View } from 'react-native';
-import * as InitDB from '../../helpers/db/initialize';
-import * as DebugDB from '../../helpers/db/debug';
-import { ThemeProvider, Button, Text } from 'react-native-elements';
-import { DevVersion } from '../../constants/Version';
+import { ThemeProvider, Text } from 'react-native-elements';
+import Animated, { Easing } from 'react-native-reanimated';
 
 import { mainTheme } from '../../constants/theme/Main';
+const {
+  set,
+  cond,
+  startClock,
+  stopClock,
+  clockRunning,
+  block,
+  timing,
+  debug,
+  Value,
+  Clock,
+  divide,
+  concat,
+} = Animated;
 
+function runTiming(clock, value, dest) {
+  const state = {
+    finished: new Value(0),
+    position: new Value(0),
+    time: new Value(0),
+    frameTime: new Value(0),
+  };
+
+  const config = {
+    duration: 5000,
+    toValue: new Value(0),
+    easing: Easing.inOut(Easing.ease),
+  };
+
+  return block([
+    cond(clockRunning(clock), 0, [
+      set(state.finished, 0),
+      set(state.time, 0),
+      set(state.position, value),
+      set(state.frameTime, 0),
+      set(config.toValue, dest),
+      startClock(clock),
+    ]),
+    timing(clock, state, config),
+    cond(state.finished, debug('stop clock', stopClock(clock))),
+    state.position,
+  ]);
+}
 type Props = { navigation: any };
 
 function DebugScreen2(props: Props) {
-  
+  const trans = runTiming(new Clock(), 0, 360);
 
   return (
-    <ThemeProvider theme={mainTheme}>
-      <View style={styles.container}>
-        <Text>Debug Screen 2</Text>
-      </View>
-    </ThemeProvider>
+    <View style={styles.container}>
+      <Animated.View
+        style={[styles.box, { transform: [{ rotate: concat(trans, 'deg') }] }]}
+      />
+      <Animated.View
+        style={[
+          styles.box,
+          {
+            transform: [
+              { rotate: concat(divide(trans, 57.2957795786), 'rad') },
+            ],
+          },
+        ]}
+      />
+    </View>
   );
 }
-
 export default DebugScreen2;
 
+const BOX_SIZE = 100;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
-  button: {
-    marginBottom: 5,
+  box: {
+    width: BOX_SIZE,
+    height: BOX_SIZE,
+    borderColor: '#F5FCFF',
+    alignSelf: 'center',
+    backgroundColor: 'plum',
+    margin: BOX_SIZE / 2,
   },
 });
