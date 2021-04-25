bf67780615418eb1af43bdf4e9fe614ac7788b2b
Antony Baasandorj <antony.baasan@gmail.com>
2021-04-25 16:16:13 -0400

    update icon

--- mobile/screens/WorkoutListScreen/WorkoutListItem.tsx
+++ mobile/screens/WorkoutListScreen/WorkoutListItem.tsx
@@ -69,7 +69,7 @@ function WorkoutListItem(props: WorkoutListItemProps) {
   const renderTooltipMenu = () => {
     return (
       <TooltipMenu height={tooltipHeight} width={tooltipWidth} items={items}>
-        <Ionicons name="settings-outline" size={32} color="black" />
+        <Ionicons name="ellipsis-vertical-circle-outline" size={32} color="black" />
       </TooltipMenu>
     );
   };
